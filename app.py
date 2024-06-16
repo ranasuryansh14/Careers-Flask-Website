e42a3ab14c14f42d6342ff86bbd2f67a7bcaba61
SURYANSH RANA <suryanshrana91@gmail.com>
2024-06-16 09:00:03 +0000

    Added Dynamic data and API route

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, jsonify
 
+app = Flask(__name__)
 
-app=Flask(__name__)
+JOBS = [
+  {
+    'id': 1,
+    'title': 'SPOC',
+    'location': 'DSW-CGC',
+    'salary': 'Rs. 1000'
+  },
+  {
+    'id': 2,
+    'title': 'President',
+    'location': 'DSW',
+    'salary': 'Rs. 1000'
+  },
+  {
+    'id': 3,
+    'title': 'President',
+    'location': 'DSW-CLUBS'
+  },
+  {
+    'id': 4,
+    'title': 'Vice President',
+    'location': 'DSW-CLUBS'
+  }
+]
 
-@app.route('/')
-def hello_world():
-    return "hello rana"
-if __name__ =="__main__":
-    app.run(host='0.0.0.0',debug=True)
+@app.route("/")
+def hello_rana():
+    return render_template('home.html', 
+                           jobs=JOBS, 
+                           company_name='CGC - DSW')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
